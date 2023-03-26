388ba6e98e077263e4b6502e2712c8f76dcaa73a
FerSoria <bercho001@gmail.com>
2023-03-26 02:02:17 -0300

    modificada la funcion de submit para llamar a diversos metodos del login service, inicialmente documentada

--- red/src/app/login.service.ts
+++ red/src/app/login.service.ts
@@ -58,4 +58,8 @@ export class LoginService {
     let user = this.getUser();
     return user.authorities[0].authority;
   }
+
+  public getCurrentUser(){
+    return this.http.get(`${this.baseUrl}/actual-user`)
+  }
 }
--- red/src/app/login/login.component.ts
+++ red/src/app/login/login.component.ts
@@ -19,13 +19,38 @@ export class LoginComponent {
   ngOnInit(): void{
 
   }
-
+/* Esta funcion es llamada al enviar un formulario de tipo login
+   llama a la funcion de login contenida en el loginService y se suscribe
+   para recibir data de tipo any que es un objeto con la propiedad token
+   y lo guarda en localstorage a traves del metodo userSession,
+   luego se suscribe a la funcion getCurrentUser para recibir un user
+   de tipo user(no tipeado todavia)
+   @param data de tipo login {username: string, password: string}
+   #post setlocalstorage token, getcurrentuser con agregado del bearer por medio
+   de un interceptor.
+*/
   formSubmit(){
     this.loginService.login(this.loginData).subscribe((data: any) => {
       console.log(data);
+      this.loginService.userSession(data.token);
+      this.loginService.getCurrentUser().subscribe((user:any) => {
+      this.loginService.setUser(user)
+      console.log(user)
+
+      if(this.loginService.getUserRole() == "ADMIN")
+      {
+        window.location.href = '=/admin';
+      } else if (this.loginService.getUserRole() == "NORMAL") {
+        window.location.href = '=""';
+      } else {
+        this.loginService.clientLogOut();
+      }
+      })
     }, (error:any) => {
       console.log(error);
     })
   }
+
+
 
 }
